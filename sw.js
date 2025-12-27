de8a1bdd7eef919c8960ec2a9dc107369a97a37e
liamihaka-blip <liamihaka@hotmail.com>
2025-12-28 00:09:05 +1100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "lostwordle-v7";
+const CACHE_NAME = "lostwordle-v8";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./sw.js"];
 
 self.addEventListener("install", (event) => {
